e539994b3b315a9005f5aea3e78a7ff6d0b68479
minhthaidj <minhthaiftu2@gmail.com>
2023-03-26 17:04:51 +0700

    Done

--- src/components/Element.js
+++ src/components/Element.js
@@ -3,7 +3,7 @@ import React from "react";
 const Element = ({ layer, index, zIndex }) => {
   return (
     <img
-      src={`character/${layer}/${index}.png`}
+      src={`character/${layer}/${index + 1}.png`}
       alt=""
       width={250}
       // Nếu dùng component thì mình nên để in line style như này ạ?
